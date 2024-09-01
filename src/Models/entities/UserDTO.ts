ac6aa7b97f3f650217714a9bd06a8b9c1a3f4da1
brennoliveira <brenno.oliveira@souunit.com.br>
2024-09-01 17:48:25 -0300

    fix: Update UserDTO model

--- src/Models/entities/UserDTO.ts
+++ src/Models/entities/UserDTO.ts
@@ -1,12 +1,12 @@
 import { IUserDTO } from "../interfaces";
 
 export class UserDTO implements IUserDTO{
-  public id?        : number;
-  public email      : string;
-  public password?  : string;
-  public balance    : number;
-  public createdAt? : Date;
-  public updatedAt? : Date;
+  id?        : number;
+  email      : string;
+  password?  : string;
+  balance    : number;
+  createdAt? : Date;
+  updatedAt? : Date;
 
   constructor(user: UserDTO) {
     this.id        = user.id;
