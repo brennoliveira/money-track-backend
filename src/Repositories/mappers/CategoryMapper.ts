b7750d0013dec4580bc7033c60024b46d682575e
brennoliveira <brenno.oliveira@souunit.com.br>
2024-12-15 15:24:43 -0300

    fix: Add id column to Category DTO

--- src/Repositories/mappers/CategoryMapper.ts
+++ src/Repositories/mappers/CategoryMapper.ts
@@ -1,10 +1,11 @@
-import { Category as CategoryPrisma } from "@prisma/client"
+import { Categories as CategoryPrisma } from "@prisma/client"
 import { CategoryDTO } from "../../Models/entities";
 import { Mapper } from "./IMapper";
 
 export class CategoryMapper implements Mapper<CategoryDTO, CategoryPrisma> {
   toDTO(persistence: CategoryPrisma): CategoryDTO {
     return new CategoryDTO({
+      id        : persistence.id,
       name      : persistence.name,
       createdAt : persistence.createdAt,
       userId    : persistence.userId,
