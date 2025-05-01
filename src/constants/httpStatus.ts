f0bb0166466d637b19aaaca54d679aac841e99eb
brennoliveira <brenno.oliveira@souunit.com.br>
2025-05-01 20:21:34 -0300

    feat: Add httpStatus constant

---
+++ src/constants/httpStatus.ts
@@ -0,0 +1,14 @@
+export const HTTP_STATUS = {
+    SUCCESS: {
+        OK         : 200,
+        CREATED    : 201,
+        NO_CONTENT : 204,
+    },
+    ERROR: {
+        BAD_REQUEST           : 400,
+        UNAUTHORIZED          : 401,
+        FORBIDDEN             : 403,
+        NOT_FOUND             : 404,
+        INTERNAL_SERVER_ERROR : 500,
+    },
+}
